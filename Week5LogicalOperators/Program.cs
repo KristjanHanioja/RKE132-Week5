23aeabf2b229615d3b695e6f4425adb0aab787a8
KristjanH <kristjan.hanioja@tktk.ee>
2024-04-23 11:33:55 +0300

    add week5 code

---
+++ Week5LogicalOperators/Program.cs
@@ -0,0 +1,12 @@
+﻿// AND - &&
+//true && true --> true
+//true && false --> false
+//false && true --> false
+//false && false --> false
+
+// OR - // 
+
+//true or true --> true
+//true or false --> true
+//false or true --> true
+//false or false --> false
